2be7d0a1bbefea5d365b47f073fd841ae46b0f79
Quentin CORDIERO <quentincordiero@FILAB.local>
2024-07-02 14:19:27 +0200

    creation du projet

---
+++ etabs/Etabs/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Etabs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ etabs/Etabs/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Etabs
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
